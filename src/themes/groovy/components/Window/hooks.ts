75c4feea52edb9926bd27370d0d2b04fca3f769e
Gnome <68836321+gnomelabs@users.noreply.github.com>
2021-06-09 00:15:44 -0700

    fixed nextInLine tab and added thing where you can hold down ctrl/cmd to open tabs in the background

--- src/themes/groovy/components/Window/hooks.ts
+++ src/themes/groovy/components/Window/hooks.ts
@@ -34,27 +34,52 @@ export const useRandomizeWindow = (localRef) => {
 }
 
 export const useTabs = (baseTab) => {
+  const [openInBackground, setOpenInBackground] = useState<boolean>(false);
   const [tabs, setTabs] = useState<string[]>([baseTab]);
+  const [justOpened, setJustOpened] = useState<string>(null);
+  // ^^ workaround to allow tabs to open in the background; openTab function is called from Tab component which doesn't have access to the latest openInBackground state, so openInBackground is always false inside openTab :(  todo see if there's a better way
   const [activeTab, setActiveTab] = useState<string>(baseTab);
   const closeTab = useCallback((tab) => {
     if (tab === activeTab) {
       const index = tabs.indexOf(tab);
-      const nextInLine = tabs[index + 1] ?? baseTab;
+      const nextInLine = tabs[index + 1] ?? tabs[index - 1] ?? baseTab;
       setActiveTab(nextInLine);
     }
     setTabs(mutateArray((array) => {
       const index = array.indexOf(tab);
       return array.splice(index, 1);
     }));
-  }, [activeTab]);
+  }, [activeTab, tabs]);
   const openTab = (tab) => {
     if (tabs.includes(tab)) {
       setActiveTab(tab);
     } else {
       setTabs(mutateArray((array) => array.push(tab)));
-      setActiveTab(tab); // todo - add thing where you can hold down ctrl or command while clicking to open tab without switching to it
+      setJustOpened(tab);
     }
   }
+  useEffect(() => {
+    if (!justOpened) return;
+    if (!openInBackground) {
+      setActiveTab(justOpened);
+    }
+    setJustOpened(null);
+  }, [openInBackground, justOpened]);
+  useEffect(() => {
+    // todo double check this isnt causing bugs if multiple useTabs <Window>s are onscreen
+    const handleKeydown = (e) => {
+      if (e.key === 'Meta') setOpenInBackground(true);
+    }
+    const handleKeyup = (e) => {
+      if (e.key === 'Meta') setOpenInBackground(false);
+    }
+    window.addEventListener('keydown', handleKeydown);
+    window.addEventListener('keyup', handleKeyup);
+    return () => {
+      window.removeEventListener('keydown', handleKeydown);
+      window.removeEventListener('keyup', handleKeyup);
+    }
+  }, []);
   return {
     tabs, openTab, closeTab,
     activeTab, setActiveTab
