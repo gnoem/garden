348e0a342e574f9210a1cd191eeef1272b504863
Gnome <68836321+gnomelabs@users.noreply.github.com>
2021-09-01 23:08:29 -0700

    make it work with touch devices

--- src/lib/three/WatchCursorControls.ts
+++ src/lib/three/WatchCursorControls.ts
@@ -11,23 +11,32 @@ class WatchCursorControls extends THREE.EventDispatcher {
   disconnect: () => void;
 	
 	constructor(camera: THREE.Camera) {
-
-		super();
-
+    
+    super();
+    
     this.isConnected = false;
     this.objects = {};
     this.userData = {};
 
-    const handleMouseMove = (e: MouseEvent): void => {
+    let objects = Object.values(this.objects);
+
+    const handleMouseMove = (e: MouseEvent | TouchEvent): void => {
+      let mouseX, mouseY;
+      if (e.type === 'touchmove') {
+        mouseX = (e as TouchEvent).touches[0].pageX;
+        mouseY = (e as TouchEvent).touches[0].pageY;
+      } else {
+        mouseX = (e as MouseEvent).clientX;
+        mouseY = (e as MouseEvent).clientY;
+      }
       // btw this stops working properly if you change the camera position/quaternion
-      const objects = Object.values(this.objects);
       if (!objects.length) return;
       const plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
       const raycaster = new THREE.Raycaster();
       const mouse = new THREE.Vector2();
       const pointOfIntersection = new THREE.Vector3();
-      mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
-      mouse.y = - (e.clientY / window.innerHeight) * 2 + 1;
+      mouse.x = (mouseX / window.innerWidth) * 2 - 1;
+      mouse.y = - (mouseY / window.innerHeight) * 2 + 1;
       raycaster.setFromCamera(mouse, camera);
       raycaster.ray.intersectPlane(plane, pointOfIntersection);
       pointOfIntersection.z = camera.position.z;
@@ -38,18 +47,21 @@ class WatchCursorControls extends THREE.EventDispatcher {
 
     this.add = (object: SceneObject): void => {
       this.objects[object.name] = object;
+      objects = Object.values(this.objects);
     }
 
     this.connect = (): void => {
       if (this.isConnected) return;
       console.log('connect')
       window.addEventListener('mousemove', handleMouseMove, false);
+      window.addEventListener('touchmove', handleMouseMove, false);
       this.isConnected = true;
     }
     
     this.disconnect = (): void => {
       if (!this.isConnected) return;
       window.removeEventListener('mousemove', handleMouseMove, false);
+      window.removeEventListener('touchmove', handleMouseMove, false);
       this.isConnected = false;
     }
 
