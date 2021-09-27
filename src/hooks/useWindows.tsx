1f94099308a647b24ae9810cbb1d83f674e943b8
Gnome <68836321+gnomelabs@users.noreply.github.com>
2021-09-26 23:03:26 -0700

    remove redundant activeWindow state in useWindows

--- src/hooks/useWindows.tsx
+++ src/hooks/useWindows.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useState } from "react";
 
-import { mutateArray } from "@utils";
+import { mutateArray, mutateStateArray } from "@utils";
 import { Window } from "../components";
 import * as siteSections from "@content/sections";
 
@@ -9,27 +9,20 @@ const getSection = (name: string) => siteSections[name.split(' ').join('')];
 const useWindows = () => {
   const [windows, setWindows] = useState<string[]>([]);
   const [windowRefs, setWindowRefs] = useState<{ [key: string]: HTMLDivElement | null }>({});
-  const [activeWindow, setActiveWindow] = useState<string | null>(null);
+  // why do these have to be possibly null? can't we create a cleanup function for each window and pass it in as a prop to return in useEffect
 
-  // when activeWindow changes, open up or switch focus to that window
-  useEffect(() => {
-    const showWindow = (name: string) => {
-      if (!getSection(name)) return null;
-      const windowIndex = windows.indexOf(name);
-      if (windowIndex === -1) { // if not in array, add to top of array
-        setWindows(mutateArray((array) => array.push(name)));
-      } else { // else, move to last position in array (window z-index will correspond to index)
-        const moveArrayElementToEnd = (array, index) => {
-          array.push(array.splice(index, 1)[0]);
-        }
-        setWindows(mutateArray((array) => moveArrayElementToEnd(array, windowIndex)));
+  const setActiveWindow = (name: string): void => {
+    if (!getSection(name)) return;
+    const indexOfActiveWindow = windows.indexOf(name);
+    if (indexOfActiveWindow < 0) {
+      setWindows(mutateStateArray(array => array.push(name)));
+    } else {
+      const moveArrayElementToEnd = (array, index) => {
+        array.push(array.splice(index, 1)[0]);
       }
+      setWindows(mutateStateArray(array => moveArrayElementToEnd(array, indexOfActiveWindow)));
     }
-
-    if (activeWindow) {
-      showWindow(activeWindow);
-    }
-  }, [activeWindow]);
+  }
 
   // anytime windowRefs changes, clean up any null windowRefs
   useEffect(() => {
@@ -41,15 +34,6 @@ const useWindows = () => {
       }
     }
   }, [Object.values(windowRefs)]);
-
-  // make sure that activeWindow is consistent with the last element in windows array
-  useEffect(() => {
-    if (!activeWindow) return;
-    if (!windows.includes(activeWindow)) {
-      const lastIndex = windows.length - 1;
-      setActiveWindow(windows[lastIndex]);
-    }
-  }, [windows]); // do NOT add activeWindow to dep array
 
   // create the actual windows
   const content = useMemo(() => {
